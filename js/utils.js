124f827bb00709c1c3fba9048aae2f6dc3a4ca02
unknown <nduflot@dawan.fr>
2022-12-13 16:00:02 +0100

    js les cookies

--- js/utils.js
+++ js/utils.js
@@ -5,3 +5,33 @@ function loaded(callable){
 function qS(selector){
     return document.querySelector(selector);
 }
+
+/* création de cookies */
+
+/**
+ * @param {string} name
+ * @param {string} value
+ * @param {number} days
+ */
+function setCookie(name, value = null, days = -1){
+    let maxAge = days * 24 *60 * 60;
+    document.cookie = `${name}=${value}; max-age=${maxAge}; Samesite=Strict; Secure`;
+}
+
+/* récupérer un cookie s'il existe */
+/**
+ * @param {string} name
+ */
+function getCookie(name){
+    let tabCookie = document.cookie.split('; ');
+    //console.log(tabCookie);
+    for(cookie of tabCookie){
+        //console.log(cookie);
+        let tabValue = cookie.split('=');
+        //console.log(tabValue);
+        if(tabValue[0] === name){
+            return tabValue[1];
+        }
+    }
+    return false;
+}
